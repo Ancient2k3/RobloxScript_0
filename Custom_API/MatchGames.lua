60deb94d3fbd2485195de249463ada61935f7b07
DE <trinhro2022@gmail.com>
2026-02-27 00:12:08 +0700

    Update MatchGames.lua

--- Custom_API/MatchGames.lua
+++ Custom_API/MatchGames.lua
@@ -5,4 +5,6 @@ local lists = {
 local url = lists[game.GameId] or ""
 if url ~= "" then
   return game:HttpGet("https://raw.githubusercontent.com/Ancient2k3/RobloxScript_0/Custom_API/Games_API/" .. url)
+else
+  return ""
 end

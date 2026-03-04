9fc2d633d7fc1cccb810c5b7be977f5720cc478e
DE <trinhro2022@gmail.com>
2026-03-04 23:41:08 +0700

    Update ElementalTycoons.lua

--- Custom_API/Games_API/ElementalTycoons.lua
+++ Custom_API/Games_API/ElementalTycoons.lua
@@ -2,6 +2,10 @@ local ws = game:GetService("Workspace")
 local plrs = game:GetService("Players")
 local rls = game:GetService("ReplicatedStorage")
 
+local type_of_items = {
+  "Part", "MeshPart", "BasePart"
+}
+
 local plr, magic, tycoons
 plr = plrs.LocalPlayer
 magic = rls.Remotes.DoMagic
@@ -23,5 +27,15 @@ function receive_turrets()
   end return listed
 end
 
+function receive_dollars()
+  local dollars = {}
+  for _, mesh in next, ws:GetDescendants() do
+    if table.find(type_of_items, mesh.ClassName) and mesh.Name == "Dollar" then
+      table.insert(dollars, mesh)
+    end
+  end return dollars
+end
+
 -- APIs Listed --
 receive_turrets()~Return a table storing turret model instance... <void>@
+receive_dollars()~Return a table of a bunch of dollar object... <void>@

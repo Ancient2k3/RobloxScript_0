bfe0eceae179c484bc578e8f6e86b6637bd7a608
DE <trinhro2022@gmail.com>
2026-05-04 23:26:03 +0700

    Create QuickInstances_1.lua

---
+++ Custom_API/QuickInstances_1.lua
@@ -0,0 +1,4 @@
+return {
+  ["screengui"] = "local AK = Instance.new(\"ScreenGui\", EF)\nNLSAK.Name = \"Name?\"",
+  ["frame"] = "local AK = Instance.new(\"Frame\", EF)\nAK.Name = \"Name?\"\nAK.BackgroundTransparency = 0\nAK.BackgroundColor3 = Color3.fromRGB(0, 0, 0)\nAK.Position = UDim2.new(0.5, 0, 0.45, 0)\nAK.Size = UDim2.new(0.05, 0, 0.1, 0)\nAK.Active = true\nAK.Draggable = false\nAK.Visible = true\n"
+}

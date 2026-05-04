38ffb0988849ebacc3862822714118acb5fb0ea4
DE <trinhro2022@gmail.com>
2026-05-04 18:24:20 +0700

    Update QuickInstances.lua

--- Custom_API/QuickInstances.lua
+++ Custom_API/QuickInstances.lua
@@ -1,3 +1,4 @@
 return {
-  ["screengui"] = "local -Name- = Instance.new(\"ScreenGui\", -Parent-)\n-Name-.Name = \"Name?\""
+  ["screengui"] = "local -Name- = Instance.new(\"ScreenGui\", -Parent-)\n-Name-.Name = \"Name?\"",
+  ["frame"] = "local -Name- = Instance.new(\"Frame\", -Parent-)\n-Name-.Name = \"Name?\"\n-Name-.BackgroundTransparency = 0\n-Name-.BackgroundColor3 = Color3.fromRGB(0, 0, 0)\n-Name-.Position = UDim2.new(0.5, 0, 0.45, 0)\n-Name-.Size = UDim2.new(0.05, 0, 0.1, 0)\n-Name-.Active = true\n-Name-.Draggable = false\n-Name-.Visible = true\n"
 }

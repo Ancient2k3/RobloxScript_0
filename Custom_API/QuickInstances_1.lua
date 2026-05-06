48dcbf4f89d60aa60b89d0b43ff196fa3736b7d6
DE <trinhro2022@gmail.com>
2026-05-06 11:31:44 +0700

    Update QuickInstances_1.lua
    updt more instances.

--- Custom_API/QuickInstances_1.lua
+++ Custom_API/QuickInstances_1.lua
@@ -1,4 +1,7 @@
 return {
-  ["screengui"] = "local AK = Instance.new(\"ScreenGui\", EF)\nNLSAK.Name = \"Name?\"",
+  ["animation"] = "local AK = Instance.new(\"Animation\", EF)\nNLSAK.Name = \"Name?\"\nNLSAK.AnimationId = \"rbxassetid://1234567890\"\nNLS",
+  ["sound"] = "local AK = Instance.new(\"Sound\", EF)\nNLSAK.Name = \"Name?\"\nNLSAK.SoundId = \"rbxassetid://1234567890\"\nNLS",
+  ["part"] = "local AK = Instance.new(\"Part\", EF)\nNLSAK.Name = \"Name?\"\nNLSAK.Material = Enum.Material.Plastic or \"Plastic\"\nNLSAK.Anchored = true\nNLSAK.CanCollide = true\nNLSAK.Position = Vector3.new(0, 0, 0)\nNLSAK.Size = Vector3.new(1, 1, 1)\nNLSAK.Transparency = 0\nNLS",
+  ["screengui"] = "local AK = Instance.new(\"ScreenGui\", EF)\nNLSAK.Name = \"Name?\"\nNLS",
   ["frame"] = "local AK = Instance.new(\"Frame\", EF)\nNLSAK.Name = \"Name?\"\nNLSAK.BackgroundTransparency = 0\nNLSAK.BackgroundColor3 = Color3.fromRGB(0, 0, 0)\nNLSAK.Position = UDim2.new(0.5, 0, 0.45, 0)\nNLSAK.Size = UDim2.new(0.05, 0, 0.1, 0)\nNLSAK.Active = true\nNLSAK.Draggable = false\nNLSAK.Visible = true\nNLSAK.ZIndex = 0\nNLS"
 }

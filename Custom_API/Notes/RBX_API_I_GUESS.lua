6f31e816639ee6f1dc93fefa5c1311d4c2314b15
DE <trinhro2022@gmail.com>
2026-05-30 22:07:20 +0700

    Create RBX_API_I_GUESS.lua

---
+++ Custom_API/Notes/RBX_API_I_GUESS.lua
@@ -0,0 +1,13 @@
+-- Emotes Catalog API --
+https://catalog.roblox.com/v1/search/items/details?Category=12
+&Subcategory=39
+&Limit=30
+&SortType=Relevance
+&SortAggregation=Ascending
+&cursor= next_page_id
+
+-- Servers API --
+https://games.roblox.com/v1/games/1234567890/servers/Public?sortOrder=Desc
+&limit=100
+&excludeFullGames=true
+&cursor= next_page_id

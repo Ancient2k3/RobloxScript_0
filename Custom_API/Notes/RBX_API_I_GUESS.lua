7907b002648389a298afbf1755e9db5744e1ecbf
DE <trinhro2022@gmail.com>
2026-06-01 11:07:16 +0700

    Update RBX_API_I_GUESS.lua

--- Custom_API/Notes/RBX_API_I_GUESS.lua
+++ Custom_API/Notes/RBX_API_I_GUESS.lua
@@ -1,5 +1,6 @@
 -- Emotes Catalog API --
-https://catalog.roblox.com/v1/search/items/details?Category=12
+https://catalog.roblox.com/v1/search/items/details?
+Category=12
 &Subcategory=39
 &Limit=30
 &SortType=Relevance
@@ -7,7 +8,8 @@ https://catalog.roblox.com/v1/search/items/details?Category=12
 &cursor= next_page_id
 
 -- Servers API --
-https://games.roblox.com/v1/games/1234567890/servers/Public?sortOrder=Desc
+https://games.roblox.com/v1/games/1234567890/servers/Public?
+sortOrder=Desc
 &limit=100
 &excludeFullGames=true
 &cursor= next_page_id

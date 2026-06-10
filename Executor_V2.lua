3bc746edf2d3f3ee7c0369d8567a1af5a5796b04
DE <trinhro2022@gmail.com>
2026-06-10 07:43:08 +0700

    Update Executor_V2.lua

--- Executor_V2.lua
+++ Executor_V2.lua
@@ -775,7 +775,7 @@ add_info_labels({
 add_display_label("Modules")
 add_info_labels({
   ["Encoder"] = ".encode(<string>), .decode(<string>)",
-  ["Map"] = ".save_map(<path>, <basepart>), .load_map(<parent>, <json_string>, <basepart>)"
+  ["Map"] = ".save_map(<path>), .load_map(<parent>, <json_string>)"
 })
 add_display_label("Code Editor Version: " .. tostring(ui_data.vars.script_version))
 add_inst_label(srvs) removing_shades()

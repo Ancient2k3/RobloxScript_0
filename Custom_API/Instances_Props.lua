20d4854d3893e7191cdb5c83a4d867afee29b708
DE <trinhro2022@gmail.com>
2026-08-04 23:06:15 +0700

    Refactor property handling for various types

--- Custom_API/Instances_Props.lua
+++ Custom_API/Instances_Props.lua
@@ -3,17 +3,25 @@ local z = function(i, p)
   local prop = i[p]
   if prop then
     local ist = typeof(i[p]):lower()
-    if ist == "vector3" then
+    if ist:match"vector3" then
       return "X: " .. tostring(prop.X):sub(1, 4) .. ", Y: " .. tostring(prop.Y):sub(1, 4) .. ", Z: " .. tostring(prop.Z):sub(1, 4)
-    elseif ist == "color3" then
-      return "R: " .. tostring(prop.R):sub(1, 4) .. ", G: " .. tostring(prop.G):sub(1, 4) .. ", B: " .. tostring(prop.B):sub(1, 4)
-    elseif ist == "cframe" then
+    elseif ist:match"color3" then
+      return "R: " .. tostring(prop.R * 255):sub(1, 4) .. ", G: " .. tostring(prop.G * 255):sub(1, 4) .. ", B: " .. tostring(prop.B * 255):sub(1, 4)
+    elseif ist:match"cframe" then
       return tostring(prop)
+    elseif ist:match"udim2" then
+      return "X: {" .. tostring(prop.X.Scale):sub(1, 4) .. ", " .. tostring(prop.X.Offset):sub(1, 4) .. "}, Y: {" .. tostring(prop.Y.Scale):sub(1, 4) .. ", " .. tostring(prop.Y.Offset):sub(1, 4) .. "}"
     end
   end return tostring(nil)
 end
 return {
   ["Folder"] = {},
+  ["ScreenGui"] = {
+    ["Enable"] = x
+  },
+  ["Frame"] = {
+    ["BackgroundTransparency"] = x, ["BackgroundColor3"] = z, ["Position"] = z, ["Size"] = z, ["Active"] = x, ["Draggable"] = x, ["Visible"] = x, ["ZIndex"] = x
+  },
   ["Sound"] = {
     ["SoundId"] = x, ["Pitch"] = x, ["Volume"] = x
   },
@@ -27,7 +35,8 @@ return {
     ["Transparency"] = x, ["Position"] = z, ["Size"] = z, ["Anchored"] = x, ["CanCollide"] = x, ["Color"] = z, ["CFrame"] = z
   },
   ["Player"] = {
-    ["UserId"] = x, ["Age"] = x, ["Archivable"] = x
+    ["UserId"] = x, ["AccountAge"] = x, ["Archivable"] = x, ["Team"] = x
   },
-  ["Model"] = {}, ["Workspace"] = {}, ["RemoteEvent"] = {}
+  ["Model"] = {}, ["Workspace"] = {}, ["RemoteEvent"] = {}, ["Players"] = {},
+  ["ReplicatedStorage"] = {}, ["Tool"] = {}
 }
